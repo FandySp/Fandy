2b36be4084f6c4db8b7626c43acd6a1b87f48c81
FandySp <fandy_suryapratama@hotmail.com>
2016-02-12 00:49:04 +0700

    perbaiki hello.php

--- hello.php
+++ hello.php
@@ -3,11 +3,11 @@
 	<title>Web App Sederhana</title>
 </head>
 	<?php
-	if(!empty($_POST)) {
+	if(!empty($_POST)){
 		$nama = $_POST['nama'];
 		$email = $_POST['email'];
 
-		echo '<h1>Halo, '. $nama . '!</h1>;
+		echo '<h1>Halo, '. $nama . '!</h1>';
 		echo 'Email : ' . $email;
 	}
 	?>
