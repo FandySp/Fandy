5908e5589d0aba196c52d641c7b7759246ad55dc
FandySp <fandy_suryapratama@hotmail.com>
2016-02-12 01:14:51 +0700

    Ubah hello.php & tambah connect.php Menghubungkan ke database

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "ap-cdbr-azure-east-c.cloudapp.net";
+$username = "b4e4227c5f49a4";
+$password = "c85acd0d":
+$database_name = "db_fandysp-websample";
+
+$conn = mysql_connect($hostname, $username, $password)
+	or die("Error menghubungkan ke host database");
+$db = mysql_select_db($database_name)
+	or die("Error menghubungkan ke database");
+?>
